b2ad7087cced166534d62247f820b4a50dab5b31
Panas Miłosz <milosz@linacti.pl>
2019-04-17 17:37:57 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
 import React, { Component } from "react";
-
 import { Search } from "./components/Search";
 import Table from "./components/Table";
-
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
+const DEFAULT_QUERY = "react";
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search?";
+const PARAM_SEARCH = "query=";
+const url = `${PATH_BASE}${PATH_SEARCH}${PARAM_SEARCH}${DEFAULT_QUERY}`;
+
+// const list = [
+//   {
+//     title: "React",
+//     url: "https://reactjs.org/",
+//     author: "Jordan Walke",
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0
+//   },
+//   {
+//     title: "Redux",
+//     url: "https://redux.js.org/",
+//     author: "Dan Abramov, Andrew Clark",
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1
+//   }
+// ];
+
 
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   state = {
-    list,
-    searchTerm: ""
+    // list,
+    result: null,
+    searchTerm: DEFAULT_QUERY
+  };
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    console.log(url);
+    fetch(`${PATH_BASE}${PATH_SEARCH}${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(error => error);
+  }
+
+  setSearchTopStories = result => {
+    this.setState({ result });
   };
 
   onDismiss = id => {
@@ -46,7 +65,11 @@ class App extends Component {
   };
 
   render() {
-    const { list, searchTerm } = this.state;
+    const { result, searchTerm } = this.state;
+
+    if (!result) {
+      return null;
+    }
 
     return (
       <div className="page">
@@ -55,7 +78,7 @@ class App extends Component {
             Search
           </Search>
           <Table
-            list={list}
+            list={result.hits}
             searchTerm={searchTerm}
             isSearched={isSearched}
             onDismiss={this.onDismiss}
