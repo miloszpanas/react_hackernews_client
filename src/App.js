5bce4b6ad82215dc828416c6f9c9201f0216803a
Panas Miłosz <milosz@linacti.pl>
2019-04-17 14:52:52 +0200

    restarting the app

--- src/App.js
+++ src/App.js
@@ -1,93 +1,59 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
-import Table from "./components/Table";
-import Button from "./components/Button";
-// api constants imported from API component;
-import { DEFAULT_QUERY, PATH_BASE, PATH_SEARCH, PARAM_SEARCH, PARAM_PAGE } from "./API";
 import "./App.css";
 
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      result: null,
-      searchTerm: DEFAULT_QUERY
-    };
-  }
-
-  fetchSearchTopStories = (searchTerm, page = 0) => {
-    fetch(`${PATH_BASE}${PATH_SEARCH}${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
-      .then(response => response.json())
-      .then(result => this.setSearchTopStories(result))
-      .catch(error => error);
-  }
-
-  componentDidMount() {
-    const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
-  }
-
-  onSearchSubmit = (event) => {
-    const { searchTerm } = this.state;
-    this.fetchSearchTopStories(searchTerm);
-    event.preventDefault();
-  }
-
-  setSearchTopStories(result) {
-    const { hits, page } = result;
-
-    const oldHits = page !== 0 ? this.state.result.hits : [];
-
-    const updatedHits = [
-      ...oldHits,
-      ...hits
-    ]
-    this.setState({ result: { hits: updatedHits, page } });
-  }
+  state = {
+    list
+  };
 
   onDismiss = id => {
-    const { result } = this.state;
-    const isNotId = item => item.objectID !== id;
-    const updatedHits = result.hits.filter(isNotId);
+    const { list } = this.state;
+    const updatedList = list.filter(item => item.objectID !== id);
     this.setState({
-      result: { ...result, hits: updatedHits }
+      list: updatedList
     });
   };
 
-  onInputChange = e => {
-    this.setState({ searchTerm: e.target.value });
-  };
-
   render() {
-    const { result, searchTerm } = this.state;
-    const page = (result && result.page) || 0;
-    console.log("pokaz stan", this.state);
-
     return (
-      <div className="page">
-        <div className="interactions">
-          <Search 
-            value={searchTerm}
-            onChange={this.onInputChange}
-            onSubmit={this.onSearchSubmit}
-          >
-            Search
-          </Search>
-        </div>
-        {result && (
-          <Table
-            list={result.hits}
-            onDismiss={this.onDismiss}
-          />
-        )}
-        <div className="interactions">
-          <Button
-            onClick={() => this.fetchSearchTopStories(searchTerm, page +1)}
-          >
-            More
-          </Button>
-        </div>
+      <div className="App">
+        {this.state.list.map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                type="button"
+                onClick={() => this.onDismiss(item.objectID)}
+              >
+                Dimiss
+              </button>
+            </span>
+          </div>
+        ))}
       </div>
     );
   }
