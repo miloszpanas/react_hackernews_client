2bdf8ca37c5808253a2542bb314081edbe2d7d02
Panas Miłosz <milosz@linacti.pl>
2019-05-26 16:02:43 +0200

    small conditional css switch with classnames

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,10 +1,18 @@
 import React from "react";
+import classNames from "classnames";
 import Button from "./Button";
 
-const Sort = ({ sortKey, onSort, children, activeSortKey }) => (
-  <Button onClick={() => onSort(sortKey)} className="button-inline">
-    {children}
-  </Button>
-);
+const Sort = ({ sortKey, onSort, children, activeSortKey }) => {
+  const sortClass = classNames(
+    "button-inline",
+    { "button-active": sortKey === activeSortKey }
+  );
+
+  return (
+    <Button onClick={() => onSort(sortKey)} className={sortClass}>
+      {children}
+    </Button>
+  );
+};
 
 export default Sort;
